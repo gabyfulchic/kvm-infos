b05d3477aeb445485286098fcfbed8af534c46ee
gabyfulchic <gaby.fulchic@ynov.com>
2019-07-04 17:22:21 +0200

    Create a VM/Bridge network

---
+++ new-bridge.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#              #
+# Gaby FULCHIC #
+#  03/07/2019  #
+#              #
+
+#      For example     #
+# ./new-bridge.sh br01 #
+
+# Initialize the Bridge Interface from the
+# xml file with virsh (libvirt)
+
+if [ "$1" != "br-auto" ]
+then
+    echo "\n"
+    echo "An error will pop if your $1.xml file have not been created !"
+    echo "\n"
+fi
+
+virsh net-define $1.xml
+virsh net-start $1
+virsh net-autostart $1
+virsh net-list --all
